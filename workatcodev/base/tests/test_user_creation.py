ea41a9061124a2c910ab7f4598f68846fa0d1195
Fabio Zanetti <fczanetti@hotmail.com>
2024-04-26 11:00:37 -0300

    tests for creating/saving operator and comon user inserted

---
+++ workatcodev/base/tests/test_user_creation.py
@@ -0,0 +1,43 @@
+import pytest
+from model_bakery import baker
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+
+@pytest.fixture
+def user_operator(db):
+    """
+    Creates and returns an operator.
+    """
+    operator = baker.make(get_user_model(), is_operator=True)
+    return operator
+
+
+@pytest.fixture
+def user_not_operator(db):
+    """
+    Creates and returns a comon user.
+    """
+    operator = baker.make(get_user_model(), is_operator=False)
+    return operator
+
+
+def test_group_creation(user_operator):
+    """
+    Certifies that a group named Operators is created when creating/saving a user.
+    """
+    assert Group.objects.filter(name='Operators').exists()
+
+
+def test_user_is_operator(user_operator):
+    """
+    Certifies that a user created as operator belongs to the Operators group.
+    """
+    assert user_operator.groups.filter(name='Operators').exists()
+
+
+def test_user_is_not_operator(user_not_operator):
+    """
+    Certifies that a user created as non operator does not belong to the Operators group.
+    """
+    assert not user_not_operator.groups.filter(name='Operators').exists()
