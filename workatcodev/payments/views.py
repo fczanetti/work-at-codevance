6886817fbd6d234547f87ab7981b012611dad6cf
Fabio Zanetti <fczanetti@hotmail.com>
2024-04-29 14:08:10 -0300

    Filter implemented and translated some texts
    closes #39 #43

--- workatcodev/payments/views.py
+++ workatcodev/payments/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
 from workatcodev.payments import facade
+from workatcodev.payments.forms import FilterStatusForm
+from django.utils.translation import gettext_lazy as _
 
 
 def home(request):
-    available_payments = facade.get_available_payments()
-    context = {'available_payments': available_payments}
+    TITLES = {'A': _('Available for anticipation'),
+              'U': _('Unavailable for anticipation'),
+              'PC': _('Pending anticipation confirmation'),
+              'AN': _('Anticipated payments'),
+              'D': _('Denied anticipation')}
+    if request.method == 'POST':
+        form = FilterStatusForm(request.POST)
+        if form.is_valid():
+            status = form.cleaned_data['status']
+            payments = facade.get_payments(status)
+            context = {'payments': payments, 'form': form, 'title': TITLES[status]}
+            return render(request, 'payments/home.html', context=context)
+    status = 'A'
+    payments = facade.get_payments(status=status)
+    form = FilterStatusForm()
+    context = {'payments': payments, 'form': form, 'title': TITLES[status]}
     return render(request, 'payments/home.html', context=context)
