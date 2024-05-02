05a799e28b5fca81cf8cbce477fa0cd40c707971
Fabio Zanetti <fczanetti@hotmail.com>
2024-05-02 15:28:30 -0300

    Filter available payments adjusted. Removed and adjusted some other tests because Payment.status won't be used in the future
    closes #50

--- workatcodev/payments/facade.py
+++ workatcodev/payments/facade.py
@@ -1,9 +1,22 @@
-from workatcodev.payments.models import Payment
+from datetime import date
+
+from workatcodev.payments.models import Payment, Supplier
+from django.core.exceptions import ObjectDoesNotExist
 
 
-def get_payments(status):
+def get_available_payments(user):
     """
-    Returns a list containing all payments from database based on the status chosen.
+    Returns all available payments for a specific user/supplier. Available payments
+    must have due_date greater than today and there must be no anticipations related.
     """
-    payments = Payment.objects.filter(status=status)
+    supplier = None
+    try:
+        supplier = Supplier.objects.get(user=user)
+    except ObjectDoesNotExist:
+        pass
+    today_date = date.today()
+    if supplier:
+        payments = Payment.objects.filter(due_date__gt=today_date, anticipation=None, supplier=supplier.id)
+        return list(payments)
+    payments = Payment.objects.filter(due_date__gt=today_date, anticipation=None)
     return list(payments)
--- workatcodev/payments/tests/conftest.py
+++ workatcodev/payments/tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
-from model_bakery import baker
-from workatcodev.payments.models import Payment
 from datetime import date, timedelta
+from workatcodev.payments.models import Supplier, Payment, Anticipation
+from model_bakery import baker
+from django.contrib.auth import get_user_model
 
 
 @pytest.fixture
@@ -12,6 +13,103 @@ def payment(db):
     d = date.today() + timedelta(days=5)
     payment = baker.make(Payment, due_date=d)
     return payment
+
+
+@pytest.fixture
+def user_01(db):
+    """
+    Creates and returns a user.
+    """
+    user_01 = baker.make(get_user_model(), email='supplier01_email.com')
+    return user_01
+
+
+@pytest.fixture
+def user_02(db):
+    """
+    Creates and returns a user.
+    """
+    user_02 = baker.make(get_user_model(), email='supplier02_email.com')
+    return user_02
+
+
+@pytest.fixture
+def supplier_01(db, user_01):
+    """
+    Creates and return a supplier.
+    """
+    supplier_01 = baker.make(Supplier, user=user_01)
+    return supplier_01
+
+
+@pytest.fixture
+def supplier_02(db, user_02):
+    """
+    Creates and return a supplier.
+    """
+    supplier_02 = baker.make(Supplier, user=user_02)
+    return supplier_02
+
+
+@pytest.fixture
+def payment_supplier_01(supplier_01):
+    """
+    Creates and returns a payment related to supplier_01.
+    """
+    payment_01 = baker.make(Payment, supplier=supplier_01)
+    return payment_01
+
+
+@pytest.fixture
+def client_logged_supplier_01(user_01, client):
+    """
+    Creates and returns a client logged with supplier_01 user.
+    """
+    client.force_login(user_01)
+    return client
+
+
+@pytest.fixture
+def available_payments_user_01(db, supplier_01):
+    """
+    Creates and returns a list of available payments for supplier_01.
+    """
+    due_date = date.today() + timedelta(days=5)
+    payments_list = baker.make(Payment, due_date=due_date, _quantity=3, supplier=supplier_01)
+    return payments_list
+
+
+@pytest.fixture
+def available_payments_user_02(db, supplier_02):
+    """
+    Creates and returns a list of available payments for supplier_02.
+    """
+    due_date = date.today() + timedelta(days=5)
+    payments_list = baker.make(Payment, due_date=due_date, _quantity=3, supplier=supplier_02)
+    return payments_list
+
+
+@pytest.fixture
+def unavailable_payments_user_01_due_date(db, supplier_01):
+    """
+    Creates and returns unavailable payments due to due_date.
+    """
+    due_date = date.today()
+    payment = baker.make(Payment, due_date=due_date, _quantity=2, supplier=supplier_01)
+    return payment
+
+
+@pytest.fixture
+def unavailable_payment_user_01_anticipation_created(db, supplier_01):
+    """
+    Creates and returns an unavailable payment due to anticipation related. This payment
+    will have a $1.00 value to diferentiate from others.
+    """
+    due_date = date.today() + timedelta(days=5)
+    new_due_date = str(date.today() + timedelta(days=1))
+    payment_ant = baker.make(Payment, due_date=due_date, supplier=supplier_01, value=1)
+    baker.make(Anticipation, payment=payment_ant, new_due_date=new_due_date)
+    return payment_ant
 
 
 @pytest.fixture
