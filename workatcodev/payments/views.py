6d2989a14b614bc0c7798f271e4a6613bbf697aa
Fabio Zanetti <fczanetti@hotmail.com>
2024-05-10 19:42:56 -0300

    common user accesses configured
    closes #106

--- workatcodev/payments/views.py
+++ workatcodev/payments/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from workatcodev.payments import facade
 from workatcodev.payments.forms import FilterStatusForm, AnticipationForm, NewPaymentForm
 from django.utils.translation import gettext_lazy as _
 
 from workatcodev.payments.models import Payment, Supplier
-
-
-def is_supplier_or_operator(user):
-    """
-    Block access to pages if the user
-    is not superuser, operator or supplier.
-    """
-    return (user.is_superuser or
-            user.is_operator or
-            Supplier.objects.filter(user=user).exists())
 
 
 def home(request):
@@ -44,7 +34,7 @@ def home(request):
     return render(request, 'payments/home.html', context=context)
 
 
-@user_passes_test(is_supplier_or_operator, login_url='/denied_access/')
+@permission_required('payments.add_anticipation', login_url='/denied_access/')
 def anticipation(request, id):
     payment = Payment.objects.get(id=id)
     if Supplier.objects.filter(user=request.user).exists():
@@ -66,7 +56,7 @@ def anticipation(request, id):
     return render(request, 'payments/anticipation.html', {'form': form, 'payment': payment})
 
 
-@user_passes_test(is_supplier_or_operator, login_url='/denied_access/')
+@permission_required('payments.add_payment', login_url='/denied_access/')
 def new_payment(request):
     context = {'form': NewPaymentForm()}
     supplier = None
