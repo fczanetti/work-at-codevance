2fefea8b5ff649a6885cf610236d78f5ee721cd2
Fabio Zanetti <fczanetti@hotmail.com>
2024-05-11 09:58:17 -0300

    approval page created
    closes #114

--- workatcodev/payments/urls.py
+++ workatcodev/payments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from workatcodev.payments.views import home, anticipation, new_payment
+from workatcodev.payments.views import home, anticipation, new_payment, approval
 
 app_name = 'payments'
 urlpatterns = [
     path('', home, name='home'),
     path('anticipation/<int:id>', anticipation, name='anticipation'),
+    path('approval/<int:id>', approval, name='approval'),
     path('new_payment/', new_payment, name='new_payment')
 ]
