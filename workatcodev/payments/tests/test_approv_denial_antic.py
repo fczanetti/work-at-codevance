97d382f54ca6b81d6de97b9f33cd425aa60c1a3a
Fabio Zanetti <fczanetti@hotmail.com>
2024-05-14 09:21:25 -0300

    tests approval/denial anticipation corrected
    closes #135

--- workatcodev/payments/tests/test_approv_denial_antic.py
+++ workatcodev/payments/tests/test_approv_denial_antic.py
@@ -1,28 +1,30 @@
-# from django.urls import reverse
-# from model_bakery import baker
-#
-# from workatcodev.payments.models import Anticipation
-#
-#
-# def test_approval_anticpation(client_logged_operator,
-#                               payment):
-#     """
-#     Approves an anticipation and certifies its
-#     status has changed.
-#     """
-#     antic = baker.make(Anticipation, payment=payment)
-#     client_logged_operator.post(reverse('payments:update_antic',
-#                                         kwargs={'act': 'D', 'id': payment.pk}))
-#     assert antic.status == 'A'
-#
-#
-# def test_denial_anticpation(client_logged_operator,
-#                               payment):
-#     """
-#     Denies an anticipation and certifies its
-#     status has changed.
-#     """
-#     antic = baker.make(Anticipation, payment=payment)
-#     client_logged_operator.post(reverse('payments:update_antic',
-#                                         kwargs={'act': 'D', 'id': payment.pk}))
-#     assert antic.status == 'D'
+from django.urls import reverse
+from model_bakery import baker
+
+from workatcodev.payments.models import Anticipation
+
+
+def test_approval_anticpation(client_logged_operator,
+                              payment):
+    """
+    Approves an anticipation and certifies its
+    status has changed.
+    """
+    antic = baker.make(Anticipation, payment=payment)
+    client_logged_operator.post(reverse('payments:update_antic',
+                                        kwargs={'act': 'A', 'id': antic.pk}))
+    antic.refresh_from_db()
+    assert antic.status == 'A'
+
+
+def test_denial_anticpation(client_logged_operator,
+                            payment):
+    """
+    Denies an anticipation and certifies its
+    status has changed.
+    """
+    antic = baker.make(Anticipation, payment=payment)
+    client_logged_operator.post(reverse('payments:update_antic',
+                                        kwargs={'act': 'D', 'id': antic.pk}))
+    antic.refresh_from_db()
+    assert antic.status == 'D'
