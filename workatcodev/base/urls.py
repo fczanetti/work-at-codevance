68419313c4afbefc9d66165dbd584aef802c75a4
Fabio Zanetti <fczanetti@hotmail.com>
2024-05-15 16:51:10 -0300

    login form created
    closes #146

--- workatcodev/base/urls.py
+++ workatcodev/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from workatcodev.base.views import denied_access
+from workatcodev.base.views import denied_access, Mylogin
 
 
 app_name = 'base'
 urlpatterns = [
-    path('denied_access/', denied_access, name='denied_access')
+    path('denied_access/', denied_access, name='denied_access'),
+    path('accounts/login/', Mylogin.as_view(), name='login')
 ]
--- workatcodev/base/views.py
+++ workatcodev/base/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 
 def denied_access(request):
     return render(request, 'base/denied_access.html')
+
+
+class Mylogin(LoginView):
+    template_name = 'registration/login.html'
